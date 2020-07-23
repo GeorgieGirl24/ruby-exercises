313432c2db89a5d90d1a1f5e7084e0c06055dc41
George Soderholm <georgesoderholm@gmail.com>
2020-07-22 21:33:26 -0600

    Add code to pass; work with sort_by

--- command-query/exercises/cupcake.rb
+++ command-query/exercises/cupcake.rb
@@ -1,2 +1,10 @@
 class Cupcake < Struct.new(:kind, :sugar)
+  attr_accessor :flavor, :sugar
+  def initialize(flavor, sugar)
+    @flavor = flavor
+    @sugar = sugar
+  end
+  def flavor
+    @flavor
+  end
 end
