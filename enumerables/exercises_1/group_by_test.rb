f1a3be6670bbdc33da1c6ee94fd23ec9318f92f4
George Soderholm <georgesoderholm@gmail.com>
2020-07-30 13:30:15 -0600

    Modify group by number of zeros to be more effient

--- enumerables/exercises_1/group_by_test.rb
+++ enumerables/exercises_1/group_by_test.rb
@@ -50,7 +50,9 @@ class GroupByTest < Minitest::Test
     # skip
     numbers = [1, 3, 500, 200, 4000, 3000, 10000, 90, 20, 500000]
     grouped = numbers.group_by do |number|
-      (number.digits.count) - 1
+      count_zero = number.digits.count do |digit|
+        digit == 0
+      end
     end
     expected = {0=>[1, 3], 2=>[500, 200], 3=>[4000, 3000], 4=>[10000], 1=>[90, 20], 5=>[500000]}
     assert_equal expected, grouped
@@ -61,7 +63,7 @@ class GroupByTest < Minitest::Test
     numbers = [1, 3, 503, 239, 4938, 3932, 19982, 93, 21, 501787]
     grouped = numbers.group_by do |number|
       number.digits.count
-    end 
+    end
     expected = {1=>[1, 3], 2=>[93, 21], 3=>[503, 239], 4=>[4938, 3932], 5=>[19982], 6=>[501787]}
     assert_equal expected, grouped
   end
