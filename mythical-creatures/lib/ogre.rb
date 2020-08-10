722f5cb2d705989a09d9782233fe7dd464fb280a
George Soderholm <georgesoderholm@gmail.com>
2020-08-10 17:57:48 -0600

    Rework class with one if statement

--- mythical-creatures/lib/ogre.rb
+++ mythical-creatures/lib/ogre.rb
@@ -1,54 +1,44 @@
 class Ogre
-  attr_accessor :name, :location, :swings
-  def initialize(name, location = "Swamp", swings = 0)
+  attr_accessor :name, :home, :swings, :encounter_counter
+  def initialize(name, home="Swamp")
     @name = name
-    @location = location
-    @counter = []
-    @swings = swings
-    @encounters = []
+    @home = home
+    @swings = 0
+    @encounter_counter = 0
   end
-  def home
-    location
-  end
+
   def encounter(human)
-    @encounters << human
-    @counter << human
-    human.encounter += 1
-    if @counter.count >= 3
-      human.notices_ogre = true
-      swing_at(human)
-      @counter.clear
+    human.encounter_counter += 1
+    @encounter_counter += 1
+  end
+
+  def swing_at(human)
+    if human.encounter_counter % 3 == 0
+      @swings += 1
     end
   end
-  def swing_at(human)
-      self.swings += 1
-      if swings % 2 == 0
-        human.knocked_out = true
-      end
-  end
-  def encounter_counter
-    @encounters.count
-  end
+
   def apologize(human)
-    human.knocked_out = false
+    human.got_apology = true
   end
 end
 
+
 class Human
-  attr_accessor :name, :encounter, :notices_ogre, :knocked_out
-  def initialize(name = 'Jane', encounter = 0, notices_ogre = false, knocked_out = false)
+  attr_accessor :name, :encounter_counter, :notices_ogre, :knocked_out, :got_apology
+  def initialize(name="Jane")
     @name = name
-    @encounter = encounter
-    @notices_ogre = notices_ogre
-    @knocked_out = knocked_out
+    @encounter_counter = 0
+    @notices_ogre = false
+    @knocked_out = false
+    @got_apology = false
   end
-  def encounter_counter
-    encounter
-  end
+
   def notices_ogre?
-    notices_ogre
+    @notices_ogre = @encounter_counter % 3 == 0
   end
+
   def knocked_out?
-    knocked_out
+    knocked_out = @encounter_counter % 3 == 0 && @encounter_counter.even? && @got_apology == false
   end
 end
