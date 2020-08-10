95872397281dac350712566c99d0a82382bf6c9c
George Soderholm <georgesoderholm@gmail.com>
2020-08-10 12:34:12 -0600

    Rework class has one if statement

--- mythical-creatures/lib/werewolf.rb
+++ mythical-creatures/lib/werewolf.rb
@@ -1,41 +1,41 @@
 class Werewolf
-  attr_accessor :name, :location, :human, :wolf, :hungry
-  def initialize(name, location = "London", human = true, hungry = false)
+  attr_accessor :name, :location, :human, :wolf, :hungry, :count, :victims
+  def initialize(name, location="London", human=true, wolf=false, hungry=false)
     @name = name
     @location = location
     @human = human
     @wolf = wolf
     @hungry = hungry
-    @stomach = []
+    @victims = []
+    @count = 0
   end
+
   def human?
     human
   end
-  def wolf?
-    wolf
-  end
+
   def change!
-    if self.human == true
-      self.human = false
-      self.wolf = true
-      self.hungry = true
-    elsif self.wolf == true
-      self.wolf = false
-      self.human = true
-      self.hungry = true
-    end
+    @hungry = true
+    @human = @human == false
+    @wolf = @wolf == false
   end
+
+  def wolf?
+    @wolf
+  end
+
   def hungry?
-    hungry
-  end
-  def consume(victim)
-    if human == false
-      @stomach << victim
-      p victim.status = :dead 
+    if @count > 0
       @hungry = false
     end
+    @hungry
   end
-  def count
-    @stomach.count
+
+  def consume(victim)
+    if @wolf == true
+      @count += 1
+      victim.status = :dead
+      @victims << victim
+    end
   end
 end
