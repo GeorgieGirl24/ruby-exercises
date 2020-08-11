599730bd723ce8871663f2690bf64043064c1510
George Soderholm <georgesoderholm@gmail.com>
2020-08-10 21:06:32 -0600

    Rework class with two if statements

--- mythical-creatures/lib/direwolf.rb
+++ mythical-creatures/lib/direwolf.rb
@@ -1,42 +1,49 @@
 class Direwolf
-  attr_accessor :name, :home, :size, :hunts_white_walkers
-  def initialize(name, home = "Beyond the Wall", size = "Massive", hunts_white_walkers = true)
+  attr_accessor :name, :home, :size, :starks, :wolf_protect, :hunts_white_walkers
+  def initialize(name, home='Beyond the Wall', size='Massive', hunts_white_walkers=true)
     @name = name
     @home = home
     @size = size
     @starks = []
+    @wolf_protect = []
     @hunts_white_walkers = hunts_white_walkers
   end
+
   def starks_to_protect
-    @starks
-  end
-  def protects(stark)
-    if home == stark.location && @starks.count < 2
-      @starks << stark
-      stark.safe = true
-      stark.house_words = "The North Remembers"
-      self.hunts_white_walkers = false
+    @starks.each do |stark|
+      stark.name
     end
   end
-  def hunts_white_walkers?
-    hunts_white_walkers
+
+  def protects(stark)
+    wolf_protect << name if (!wolf_protect.include?(name))
+    wolf_protect << stark.name if wolf_protect.length <= 2
+    stark.safe = @wolf_protect.include?(stark.name)
+    @starks << stark if stark.location == @home && wolf_protect.include?(stark.name)
   end
+
+  def hunts_white_walkers?
+    hunts_white_walkers = !(@wolf_protect.include?(name))
+  end
+
   def leaves(stark)
-    stark.safe = false
-    @starks.clear
+    @wolf_protect.delete_if{|element| @wolf_protect.include?(stark.name)}
+    @starks.delete_if{|stark| !(@wolf_protect.include?(stark.name))}
+    stark.safe = @wolf_protect.include?(stark.name)
     stark
   end
 end
 
 
 class Stark
-  attr_accessor :name, :location, :house_words, :safe
-  def initialize(name, location = "Winterfell", safe = false, house_words = "Winter is Coming")
+  attr_accessor :name, :location, :safe, :house_words
+  def initialize(name, location='Winterfell', safe=false, house_words='Winter is Coming')
     @name = name
     @location = location
-    @house_words= house_words
     @safe = safe
+    @house_words = house_words
   end
+
   def safe?
     safe
   end
