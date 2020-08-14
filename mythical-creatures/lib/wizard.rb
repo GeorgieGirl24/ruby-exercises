4247493fcdf34a18afd2e2e0f49d394edd1cbe89
George Soderholm <georgesoderholm@gmail.com>
2020-08-13 18:06:18 -0600

    Add dontUsecamelCasehere to work with the Journey

--- mythical-creatures/lib/wizard.rb
+++ mythical-creatures/lib/wizard.rb
@@ -22,7 +22,18 @@ class Wizard
   def cast
     @cast += 1
     "MAGIC MISSILE"
-
   end
 
+  def dontUsecamelCaseshere
+    "Hello y'all, sup sup sup, so I know what you're thinking, old
+    Josh here is coming in to save the day. Well not today, you
+    need to believe in yourself, you can totally do it!
+    *cue epic montage*
+    -Bam! you now have a new power - ~power of vim~ -
+    Hey, if we got time later, let's draw some stuff in the
+    browser.
+    The browser? you say... say what time period is this, whoops...
+    I'm outa here , outa here, outa here(*say that to yourself in a
+    echo'ey kind of way*)"
+  end
 end
