77e99c30b6c64ac15164479c254ca1b3f732c70d
George Soderholm <georgesoderholm@gmail.com>
2020-08-14 23:18:36 -0600

    Edit out unneeded attributes

--- mythical-creatures/lib/sphinx.rb
+++ mythical-creatures/lib/sphinx.rb
@@ -1,14 +1,10 @@
 class Sphinx
-  attr_accessor :name, :roman_mapping, :roman_makings, :tens, :ones
+  attr_accessor :name, 
 
   ROMAN_NUMERALS = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
 
   def initialize(name)
     @name = name
-    @roman_holdings = []
-    @roman_makings = []
-    @ones = ones
-    @tens = tens
   end
 
   def sphinx_challenge(number)
