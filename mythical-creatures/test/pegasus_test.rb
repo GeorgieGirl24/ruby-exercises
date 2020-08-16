db4bccdffbe6c1d14fd079ab179e0b610a9e09ba
George Soderholm <georgesoderholm@gmail.com>
2020-08-15 18:41:54 -0600

    Add 11 tests

--- mythical-creatures/test/pegasus_test.rb
+++ mythical-creatures/test/pegasus_test.rb
@@ -21,7 +21,7 @@ class PegasusTest < Minitest::Test
     # skip
     pegasus = Pegasus.new("Pegasus")
 
-    assert pegasus.flying
+    assert pegasus.fly
   end
 
   def test_it_can_fly
@@ -31,11 +31,11 @@ class PegasusTest < Minitest::Test
     assert pegasus.flying?
   end
 
-  def test_it_cant_rest_if_it_is_flying
+  def test_it_cant_be_grounded_if_it_is_flying
     # skip
     pegasus = Pegasus.new("Pegasus")
-
-    refute pegasus.resting?
+# require 'pry';binding.pry
+    refute pegasus.grounded?
   end
 
   def test_it_can_rest
@@ -44,7 +44,8 @@ class PegasusTest < Minitest::Test
     pegasus.land
     pegasus.take_a_nap
 
-    assert pegasus.resting?
+    refute pegasus.flying?
+    assert pegasus.grounded?
   end
 
   def test_it_is_created_in_the_mortal_realm
@@ -59,25 +60,73 @@ class PegasusTest < Minitest::Test
     pegasus = Pegasus.new("Pegasus")
     pegasus.land
     pegasus.take_a_nap
-    pegaus.journey_to_another_realm
+    pegasus.journey_to_another_realm
 
-    assert_equal "You can't travel with your feet on the ground!", pegaus.journey_to_another_realm
+    assert_equal "You can't travel with your feet on the ground!", pegasus.journey_to_another_realm
   end
+
 
   def test_it_can_travel_to_the_immortal_realm
-    skip
-  end
+    # skip
+    pegasus = Pegasus.new("Pegasus")
+    pegasus.land
+    pegasus.take_a_nap
+    pegasus.take_off
+    pegasus.fly
+    pegasus.journey_to_another_realm
 
-  def test_it_cant_be_flying_if_it_is_on_the_ground
-    skip
+    assert pegasus.immortal_realm
+    refute pegasus.mortal_realm
+    assert_equal "Time and space are no match for the amazing!", pegasus.journey_to_another_realm
   end
 
   def test_it_can_create_pools_when_it_touches_down
-    skip
+    # skip
+    pegasus = Pegasus.new("Pegasus")
+    assert [], pegasus.pools
+    blue_lagoon = Pool.new("Blue Lagoon", :medium, "Kastoria")
+    pegasus << blue_lagoon
+    pegasus.land
+    pegasus.take_off
+    refute pegasus.grounded?
+    pegasus.land
+    shimmering_lake = Pool.new("Shimmering Lake", :large, "Thessaloniki")
+    pegasus << shimmering_lake
+    pegasus.take_a_nap
+
+    assert_instance_of Pool, blue_lagoon
+    assert_instance_of Pool, shimmering_lake
+    assert_equal 2, pegasus.pools.length
   end
 
   def test_it_can_only_create_four_pools_before_rest_must_be_taken
-    skip
+    # skip
+    pegasus = Pegasus.new("Pegasus")
+    assert [], pegasus.pools
+    blue_lagoon = Pool.new("Blue Lagoon", :medium, "Kastoria")
+    pegasus << blue_lagoon
+    pegasus.land
+    pegasus.take_off
+    refute pegasus.grounded?
+    pegasus.land
+    shimmering_lake = Pool.new("Shimmering Lake", :large, "Thessaloniki")
+    pegasus << shimmering_lake
+    pegasus.take_off
+    pegasus.journey_to_another_realm
+    assert pegasus.immortal_realm
+    pegasus.journey_to_another_realm
+    refute pegasus.immortal_realm
+    pegasus.land
+    sapphire_pool = Pool.new("Sapphire Pool", :small, "Aetos")
+    pegasus << sapphire_pool
+    pegasus.take_off
+    pegasus.land
+    crown_hill = Pool.new("Crown Hill", :tiny, "Stymfalia")
+    pegasus << crown_hill
+    pegasus.take_off
+
+    refute pegasus.flying?
+    assert_equal "You MUST rest!", pegasus.take_off
   end
 
 end
