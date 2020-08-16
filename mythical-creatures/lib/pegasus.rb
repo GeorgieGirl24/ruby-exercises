0ee86964b3c3b09657097a0f9e2333c0406a218e
George Soderholm <georgesoderholm@gmail.com>
2020-08-15 18:41:15 -0600

    Add methods to pass tests

--- mythical-creatures/lib/pegasus.rb
+++ mythical-creatures/lib/pegasus.rb
@@ -1,26 +1,68 @@
 class Pegasus
-  attr_accessor :name, :flying, :resting, :mortal_realm
-  def initialize(name, flying=true, mortal_realm=true)
+  attr_accessor :name, :fly, :grounded, :mortal_realm, :pools, :count, :immortal_realm
+  def initialize(name, fly=true, mortal_realm=true, immortal_realm=false)
     @name = name
-    @flying = flying
-    @resting = resting
+    @fly = fly
+    @grounded = grounded
     @mortal_realm = mortal_realm
+    @immortal_realm = immortal_realm
+    @pools = []
+    @count = 0
   end
 
   def flying?
-    @flying = !(@resting == true)
+    @fly = !(@grounded == true)
   end
 
-  def resting?
-    @resting = !(@flying == true)
+  def grounded?
+    @grounded = !(@fly == true)
   end
 
   def land
-    @flying = false
+    @grounded = true
   end
 
   def take_a_nap
-    @resting = true
+    @grounded = true
   end
 
+  def take_off
+    if @count > 3
+      @grounded = true
+      "You MUST rest!"
+    else
+      @grounded = false
+    end
+  end
+
+  def journey_to_another_realm
+    if grounded == true
+      "You can't travel with your feet on the ground!"
+    elsif grounded == false
+      # require 'pry'; binding.pry
+      if @mortal_realm == true
+        @mortal_realm = false
+        @immortal_realm = true
+      elsif @immortal_realm == true
+        @immortal_realm = false
+        @mortal_realm = true
+      end
+      "Time and space are no match for the amazing!"
+    end
+  end
+
+  def << (pool)
+    @pools << pool
+    @count = @pools.count
+  end
+end
+
+
+class Pool
+  attr_accessor :name, :size, :location
+  def initialize(name, size, location)
+    @name = name
+    @size = size
+    @location = location
+  end
 end
